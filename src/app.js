3a7d34b3d2646a228d389f865f437fdf05ebdf00
Stephen Kurshner <skurshner@gmail.com>
2021-11-15 12:00:05 -0800

    initial commit, added cors and migration files

---
+++ src/app.js
@@ -0,0 +1,9 @@
+if (process.env.USER) require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+module.exports = app;
