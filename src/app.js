0b88dc5f473ab814bf9ab74ce7529fa804b78b94
Stephen Kurshner <skurshner@gmail.com>
2021-11-16 10:41:46 -0800

    added get route for '/theaters', all tests passing

--- src/app.js
+++ src/app.js
@@ -4,12 +4,14 @@ const cors = require("cors");
 const app = express();
 const moviesRouter = require("./movies/movies.router");
 const reviewsRouter = require("./reviews/reviews.router");
+const theatersRouter = require("./theaters/theaters.router");
 
 app.use(cors());
 app.use(express.json());
 
 app.use("/movies", moviesRouter);
 app.use("/reviews", reviewsRouter);
+app.use("/theaters", theatersRouter);
 
 // Not found handler
 app.use((req, res, next) => {
